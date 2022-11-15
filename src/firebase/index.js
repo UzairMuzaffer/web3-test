91f37342ebe9e858d9e9ad1e39bb887293f28533
uzairali-viabletree <uzai.ali@viabletree.com>
2022-11-15 13:14:49 +0500

    firebase hosting setup

---
+++ src/firebase/index.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDunNjpgzV0vYBb4nLPh0iZg4Vf55hqtt0",
+
+    authDomain: "web3test-82ee1.firebaseapp.com",
+
+    projectId: "web3test-82ee1",
+
+    storageBucket: "web3test-82ee1.appspot.com",
+
+    messagingSenderId: "641270832517",
+
+    appId: "1:641270832517:web:75ce9dd09994cb8a62f8ec"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
